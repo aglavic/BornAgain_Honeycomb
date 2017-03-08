208a5b8fd9f68c4317e26e5ff18d2a216d73a46c
Artur Glavic <artur.glavic@psi.ch>
2017-03-08 14:34:33 +0100

    Add genx SL databases and implement GISAXS, glass and ice states.

--- HoneyComb.py
+++ HoneyComb.py
@@ -30,6 +30,8 @@ lambda_i=5.5
 phi_f_min, phi_f_max=-1.9, 1.75
 alpha_f_min, alpha_f_max=0.0, 4.0
 tth_min, tth_max=-0.08, 4.4
+phix_f_min, phix_f_max=-0.8, 2.2
+tthx_min, tthx_max=0.0, 2.0
 phi_f_offspec=0.3
 
 BIN=4 # reduce resolution by this factor to speed up simulation
@@ -45,7 +47,7 @@ CHANNELS={'uu': (_up, _up),
 
 def get_simulation_gisans(channel, **kwrds):
     """
-    Create and return GISAXS simulation with beam and detector defined
+    Create and return GISANS simulation with beam and detector defined
     """
     simulation=ba.GISASSimulation()
     simulation.setDetectorParameters(248/BIN, phi_f_min*deg, phi_f_max*deg, 296/BIN,
@@ -61,6 +63,25 @@ def get_simulation_gisans(channel, **kwrds):
     simulation.setSampleBuilder(SB)
     simulation.setBeamPolarization(CHANNELS[channel][0])
     simulation.setAnalyzerProperties(CHANNELS[channel][1], 1.0, 0.5)
+
+    return simulation
+
+def get_simulation_gisaxs(**kwrds):
+    """
+    Create and return GISAXS simulation with beam and detector defined
+    """
+    simulation=ba.GISASSimulation()
+    simulation.setDetectorParameters(512/BIN, phix_f_min*deg, phix_f_max*deg, 512/BIN,
+                                    (tthx_min-alpha_i)*deg, (tthx_max-alpha_i)*deg)
+    simulation.setBeamParameters(lambda_i*A, alpha_i*deg, 0.0*deg)
+    simulation.setBeamIntensity(1e6)
+
+    if INTEGRATE_XI:
+      xi_rotation=ba.DistributionGate(-90, 90)
+      simulation.addParameterDistribution("*/SampleBuilder/xi", xi_rotation, 36)
+
+    SB=HCSample(**kwrds)
+    simulation.setSampleBuilder(SB)
 
     return simulation
 
@@ -129,12 +150,14 @@ def get_simulation_spec(channel, **kwrds):
     return simulation
 
 
-def sim_and_show_gisans(model='ferro'):
+def sim_and_show_gisans(model='ferro', save=None):
     """
     Simulate all GISANS channels and plot the result.
     """
+    print "GISANS %s at %.2fdeg and %.2fA"%(model, alpha_i, lambda_i)
+
     kwrds=dict(bias_field=MsB, edge_field=Ms, lambda_i=lambda_i,
-                magnetic_model=model, xi=lattice_rotation)
+                basic_model=model, xi=lattice_rotation)
 
     simulation=get_simulation_gisans('uu', **kwrds)
     simulation.runSimulation()
@@ -164,32 +187,94 @@ def sim_and_show_gisans(model='ferro'):
     plt.imshow(uu-dd, extent, Imin=Imin, Imax=Imax, title='FM [(++)-(--)]')
     plt.imshow((ud+du)/2., extent, Imin=Imin, Imax=Imax, title='SF [(+-)+(-+)]')
 
+    if save is not None:
+      fn=save.rsplit('.', 1)[0]+('_%.2fdeg_%.2fA.'%(lambda_i, alpha_i))+save.rsplit('.', 1)[1]
+      fh=open(fn, 'w')
+      header='# Qy Qz Iuu Idd Iud Idu'
+      fh.write(header+'\n')
+      Qy, Qz=meshgrid(linspace(2*pi/lambda_i*phi_f_min*deg, 2*pi/lambda_i*phi_f_max*deg, 248/BIN),
+                     linspace(2*pi/lambda_i*tth_min*deg, 2*pi/lambda_i*tth_max*deg, 296/BIN))
+      for items in zip(*tuple([Qy, Qz, uu[::-1], dd[::-1], ud[::-1], du[::-1]])):
+        savetxt(fh, array(list(items)).T)
+        fh.write('\n')
 
-def run_gisans(only_first=False, model='ferro'):
+
+def run_gisans(only_first=False, model='ferro', save=None):
     """
     Run simulation for GISANS and plot result
     """
-    global lattice_rotation, lambda_i, up, down
+    global lattice_rotation, lambda_i, up, down, alpha_i
     up=ba.kvector_t(0., 1., 0.)
     down=-up
 
+    alpha_i=0.35
     lambda_i=4.7
-    sim_and_show_gisans(model=model)
+    sim_and_show_gisans(model=model, save=save)
 
     if only_first:
         return
 
     lambda_i=5.4
-    sim_and_show_gisans()
+    sim_and_show_gisans(model=model, save=save)
 
     lambda_i=6.1
-    sim_and_show_gisans()
+    sim_and_show_gisans(model=model, save=save)
 
     lambda_i=6.8
-    sim_and_show_gisans()
+    sim_and_show_gisans(model=model, save=save)
 
+    alpha_i=1.4
+    lambda_i=4.7
+    sim_and_show_gisans(model=model, save=save)
 
-def run_offspec(model='ferro'):
+    lambda_i=5.4
+    sim_and_show_gisans(model=model, save=save)
+
+    lambda_i=6.1
+    sim_and_show_gisans(model=model, save=save)
+
+    lambda_i=6.8
+    sim_and_show_gisans(model=model, save=save)
+
+def run_gisaxs(ai=0.25, save=None):
+    """
+    Run simulation for GISANS and plot result
+    """
+    global lattice_rotation, lambda_i, alpha_i
+    alpha_i=ai
+    lambda_i=1.54
+
+    kwrds=dict(bias_field=MsB, edge_field=Ms, lambda_i=lambda_i,
+              hc_lattice_length=27.7*nm, hc_inner_radius=10.0*nm,
+              domain=300.*nm, cauchy=1.*nm,
+              py_d=16.0*nm, top_d=0.0*nm,
+              basic_model='xray', xi=lattice_rotation)
+
+    simulation=get_simulation_gisaxs(**kwrds)
+    simulation.runSimulation()
+    I=simulation.getIntensityData().getArray()
+
+    Imax=I.max()
+    Imin=1e-7*Imax
+
+    plt=BAPlotter(nitems=1, rows=1, title=r'GISAXS for $\lambda$=%.2f $\AA$'%lambda_i,
+                  gisans=True, xlim=(-0.05, 0.15), ylim=(0, 0.1))
+    extent=[2*pi/lambda_i*phix_f_min*deg, 2*pi/lambda_i*phix_f_max*deg,
+            2*pi/lambda_i*tthx_min*deg, 2*pi/lambda_i*tthx_max*deg]
+    # showing the result
+    plt.imshow(I, extent, Imin=Imin, Imax=Imax, title='')
+
+    if save is not None:
+      fh=open(save, 'w')
+      header='# Qy Qz Ix'
+      fh.write(header+'\n')
+      Qy, Qz=meshgrid(linspace(2*pi/lambda_i*phix_f_min*deg, 2*pi/lambda_i*phix_f_max*deg, 512/BIN),
+                     linspace(2*pi/lambda_i*tthx_min*deg, 2*pi/lambda_i*tthx_max*deg, 512/BIN))
+      for items in zip(*tuple([Qy, Qz, I[::-1]])):
+        savetxt(fh, array(list(items)).T)
+        fh.write('\n')
+
+def run_offspec(model='ferro', save=None):
     """
     Run simulation for ToF off-specular and plot results
     """
@@ -197,11 +282,17 @@ def run_offspec(model='ferro'):
     lambda_i=5.8
     alpha_i=0.3
 
-    kwrds=dict(bias_field=MsB, edge_field=Ms, magnetic_model=model)
+    qu, ru=loadtxt('data/fit_upd_model_4_000.dat').T[0:2]
+    qd, rd=loadtxt('data/fit_upd_model_4_001.dat').T[0:2]
+    ru_q=interp1d(qu, ru, bounds_error=False, fill_value=1.0)
+    rd_q=interp1d(qd, rd, bounds_error=False, fill_value=1.0)
+    Ru=[]
+    Rd=[]
+    kwrds=dict(bias_field=MsB, edge_field=Ms, basic_model=model)
 
     uu=[]; dd=[]; ud=[]; du=[]
     ais=[]; pis=[]; pfs=[]; lis=[]
-    print u'Simulating αi=%.2f'%alpha_i
+    print('Simulating αi=%.2f'%alpha_i)
     for lambda_i in linspace(7.8, 2.6, 60):
       kwrds['lambda_i']=lambda_i
       lis.append(lambda_i*ones(296/BIN))
@@ -224,7 +315,7 @@ def run_offspec(model='ferro'):
         du.append(simulation.getIntensityData().getArray()[::-1, 0])
 
     for alpha_i in [0.54, 0.97, 1.75]:
-      print u'Simulating αi=%.2f'%alpha_i
+      print('Simulating αi=%.2f'%alpha_i)
       for lambda_i in linspace(5.4, 2.6, 20):
         kwrds['lambda_i']=lambda_i
         lis.append(lambda_i*ones(296/BIN))
@@ -246,17 +337,23 @@ def run_offspec(model='ferro'):
           simulation.runSimulation()
           du.append(simulation.getIntensityData().getArray()[::-1, 0])
 
-    uu=array(uu)*(array(ais)/0.3)*array(lis)**2## Why do I need to scale by λ²?
-    dd=array(dd)*(array(ais)/0.3)*array(lis)**2##
+    uu=array(uu)*(array(ais)/0.3)#*array(lis)**2## Why do I need to scale by λ²?
+    dd=array(dd)*(array(ais)/0.3)#*array(lis)**2##
+    pis=array(pis); pfs=array(pfs)
+    Ru=ru_q(pis+pfs)*exp(-0.5*((pis-pfs)/0.0004)**2)*2e6
+    Rd=rd_q(pis+pfs)*exp(-0.5*((pis-pfs)/0.0004)**2)*2e6
+    header='# pi-pf Qz Ru Rd uu dd'
+    Intensities=[Ru, Rd, uu, dd]
     if model!='ferro':
-      ud=array(ud)*(array(ais)/0.3)*array(lis)**2## Why do I need to scale by λ²?
-      du=array(ud)*(array(ais)/0.3)*array(lis)**2##
+      ud=array(ud)*(array(ais)/0.3)#*array(lis)**2## Why do I need to scale by λ²?
+      du=array(ud)*(array(ais)/0.3)#*array(lis)**2##
+      header+=' ud du'
+      Intensities+=[ud, du]
       plt=BAPlotter(nitems=3, title=r'ToF simulations for vortex state',
                     gisans=False, xlim=(-0.05, 0.05), ylim=(0., 0.15))
     else:
       plt=BAPlotter(nitems=2, title=r'ToF simulations for ferromagnetic state',
                     gisans=False, xlim=(-0.05, 0.05), ylim=(0., 0.15))
-    pis=array(pis); pfs=array(pfs)
 
     Imax=max(uu.max(), dd.max())
     Imin=Imax*1e-7
@@ -268,12 +365,20 @@ def run_offspec(model='ferro'):
     if model!='ferro':
       plt.pcolormesh(pis-pfs, pis+pfs, (ud+du)/2., title='SF [(+-)+(-+)]', Imin=Imin, Imax=Imax)
 
+    if save is not None:
+      fh=open(save, 'w')
+      fh.write(header+'\n')
+      for items in zip(*tuple([pis-pfs, pis+pfs]+Intensities)):
+        savetxt(fh, array(list(items)).T)
+        fh.write('\n')
 
 def run_offspec_angular(model='ferro', save=None):
     """
     Run simulation for angular dispersive off-specular and plot results
     """
     global lattice_rotation, alpha_i, lambda_i
+    print "OffSpecular (angular) %s"%(model)
+
 
     lambda_i=4.0
     alpha_i=0.3
@@ -284,7 +389,7 @@ def run_offspec_angular(model='ferro', save=None):
     rd_q=interp1d(qd, rd, bounds_error=False, fill_value=1.0)
 
     kwrds=dict(bias_field=MsB, edge_field=Ms, lambda_i=lambda_i,
-                magnetic_model=model)
+                basic_model=model)
 
 
     simulation=get_simulation_offspec_angular(0.05, 2.5, 200/BIN, 'uu', **kwrds)
@@ -341,27 +446,35 @@ def run_offspec_angular(model='ferro', save=None):
         savetxt(fh, array(list(items)).T)
         fh.write('\n')
 
-
+def debug(model='ferro'):
+  s=HCSample(basic_model=model)
+  s.buildSample()
 
 
 if __name__=='__main__':
-  INTEGRATE_XI=False # should be used for actual simulation, False will speed it up very much
-  Ms=0.
-  MsB=30.
-  run_offspec_angular(model='ferro')
-  #run_offspec(model='ferro')
-  #run_offspec_angular(model='vortex') # should be run for actual simulation to get rid of differences due to unit cell size
-  Ms=20.
-  MsB=5.
-  run_offspec_angular(model='vortex') # should be run for actual simulation to get rid of differences due to unit cell size
+  #debug(model='ice-2') # call that throws actual usable python error
 
   INTEGRATE_XI=True # should be used for actual simulation, False will speed it up very much
   Ms=0.
-  MsB=30.
-  run_gisans(only_first=True, model='ferro')
-  Ms=20.
-  MsB=5.
-  run_gisans(only_first=True, model='vortex')
+  MsB=0.10e6
+  run_offspec_angular(model='ferro', save='data/offspec_ferro.dat')
+  run_offspec(model='ferro', save='data/offspec_tof_ferro.dat')
+#  Ms=1.5e6
+#  MsB=1e3
+#  run_offspec_angular(model='vortex', save='data/offspec_vortex.dat') # should be run for actual simulation to get rid of differences due to unit cell size
+#
+#  INTEGRATE_XI=True # should be used for actual simulation, False will speed it up very much
+#  Ms=0.
+#  MsB=0.03e6
+#  print "Simulating ferro model"
+#  run_gisans(only_first=True, model='ferro', save='data/GISANS_ferro.dat')
+#  Ms=1.5e6
+#  MsB=0.001e6
+#  run_gisans(only_first=False, model='glass', save='data/GISANS_glass.dat')
+#  run_gisans(only_first=True, model='ice-1', save='data/GISANS_ice-1.dat')
+#  run_gisans(only_first=True, model='ice-2', save='data/GISANS_ice-2.dat')
+#  run_gisans(only_first=False, model='vortex', save='data/GISANS_vortex.dat')
+  #run_gisaxs()
 
   # this does not work!!!
   #simulation=get_simulation_spec('uu')
--- HoneyCombSample.py
+++ HoneyCombSample.py
@@ -10,16 +10,28 @@ BornAgain version 1.7.1
 import bornagain as ba
 from ba_helper import MLBuilder
 from numpy import pi, sin, cos, sqrt
+from genx_utils import bc, fp
 
 # define some abbreviations
 deg=ba.deg;AA=ba.angstrom;nm=ba.nm
+fm=nm*10**-6
 
 # constants used for the model
-SLD_Si=2.029e-6*AA**-2 #
-SLD_Py=9.152e-6*AA**-2 # Fe0.2Ni0.8 with density 8.72 g/cm³
-SLD_Top=2.5e-6*AA**-2 # Unknown surface layer
+#SLD_Si=2.029e-6*AA**-2 #
+#SLD_Py=9.152e-6*AA**-2 # Fe0.2Ni0.8 with density 8.72 g/cm³
+#SLD_Top=2.5e-6*AA**-2 # Unknown surface layer
 s3=sqrt(3.)
-
+DENS={  # FU density dictionary
+  'Si': 0.04996*AA**-3,
+  'Py': 0.090347072*AA**-3,
+  'Top': 0.04*AA**-3,
+  }
+FU={ # fomula unit dictionary
+  'Si': ['Si'],
+  'Py': ['Ni*0.8', 'Fe*0.3'],
+  'Top': ['Ni*0.8', 'Fe*0.2'],
+  }
+RhoB0=2.853e-6*AA**-2
 
 class HCSample(MLBuilder):
   """
@@ -44,9 +56,11 @@ class HCSample(MLBuilder):
   cauchy=None
   interference_model='paracrystal'
 
+  single_uc=['xray', 'ferro', 'ice-1', 'ice-2', 'glass']
+
   def __init__(self, py_d=13.4*nm, top_d=8.0*nm,
                hc_lattice_length=35.0*nm, hc_inner_radius=13.0*nm,
-               magnetic_model='vortex', bias_field=0.0, edge_field=1.0,
+               basic_model='vortex', bias_field=0.0, edge_field=1.0,
                xi=0., domain=250.*nm, cauchy=15*nm,
                lambda_i=4.0):
     ba.IMultiLayerBuilder.__init__(self)
@@ -62,20 +76,39 @@ class HCSample(MLBuilder):
     # rotation of the lattice to the incident beam
     self._xi=self._registerParameter('xi', xi)
     # normal python attributes
-    self.magneti_model=magnetic_model
+    self.basic_model=basic_model
     self.bias_field=bias_field
     self.edge_field=edge_field
     self.lambda_i=lambda_i
     self.domain=domain
     self.cauchy=cauchy
+    if basic_model=='xray':
+      self.bias_field=0.
+      self.edge_field=0.
+
+  def get_n(self, item):
+    '''
+    Calculate beta and gamma depending on material, wavelength and model
+    '''
+    wl=(self.lambda_i*AA)**2/2./pi
+    if item=='MagB':
+      return self.bias_field*wl*RhoB0
+    if item=='MagE':
+      return self.edge_field*wl*RhoB0
+    if self.basic_model=='xray':
+      fp.set_wavelength(self.lambda_i)
+      itm='fp.%s'
+    else:
+      itm='bc.%s'
+    SLD=0.j
+    for element in FU[item]:
+      SLD+=DENS[item]*eval(itm%element)*fm
+    return wl*SLD
 
   def buildSample(self):
     '''
     Constructs the sample for current values of parameters.
     '''
-    sf=(self.lambda_i*AA)**2/2./pi
-    #print self.lambda_i, SLD_Py*sf
-
     # the magnetic unti cell lattice length, 30degree tilted, sqrt(3)xsqrt(3)
     self.mag_lattice_length=self.hc_lattice_length*s3
     # calculate the surface filling density from the lattice size and cylinder
@@ -86,11 +119,14 @@ class HCSample(MLBuilder):
 
     # define materials used in the model
     m_air=ba.HomogeneousMaterial("Air", 0.0, 0.0)
-    m_substrate=ba.HomogeneousMaterial("Silicon", SLD_Si*sf, 0.)
+    m_substrate=ba.HomogeneousMaterial("Silicon", self.get_n('Si').real, self.get_n('Si').imag)
     # average density of the Py layer
-    m_layer=ba.HomogeneousMagneticMaterial("PermalloyLayer", self.surface_fraction*SLD_Py*sf, 0.,
-                                           self.surface_fraction*B_ext*self.bias_field*sf)
-    m_top=ba.HomogeneousMaterial("SurfaceLayer", self.surface_fraction*SLD_Top*sf, 0.)
+    m_layer=ba.HomogeneousMagneticMaterial("PermalloyLayer",
+                                           self.surface_fraction*self.get_n('Py').real,
+                                           self.surface_fraction*self.get_n('Py').imag,
+                                           self.surface_fraction*B_ext*self.get_n('MagB'))
+    m_top=ba.HomogeneousMaterial("SurfaceLayer", self.surface_fraction*self.get_n('Top').real,
+                                 self.surface_fraction*self.get_n('Top').imag)
 
     # initialize model and ambiance layer
     multi_layer=ba.MultiLayer()
@@ -110,8 +146,22 @@ class HCSample(MLBuilder):
     particle_layout=self.build_lattice()
     py_layer=ba.Layer(m_layer, self.py_d)
     py_layer.addLayout(particle_layout)
+
+    # add incoherent magnetic part for certain models
+    surface_density=particle_layout.getTotalParticleSurfaceDensity()
+    if self.basic_model=='glass':
+      for PLi in self.build_glass(hexagon_density=surface_density):
+        py_layer.addLayout(PLi)
+    if self.basic_model=='ice-1':
+      for PLi in self.build_ice1(hexagon_density=surface_density):
+        py_layer.addLayout(PLi)
+    if self.basic_model=='ice-2':
+      for PLi in self.build_ice2(hexagon_density=surface_density):
+        py_layer.addLayout(PLi)
+
     #multi_layer.addLayerWithTopRoughness(py_layer, roughness)
     multi_layer.addLayer(py_layer)
+
     substrate_layer=ba.Layer(m_substrate, 0)
     #multi_layer.addLayerWithTopRoughness(substrate_layer, roughness)
     multi_layer.addLayer(substrate_layer)
@@ -126,15 +176,14 @@ class HCSample(MLBuilder):
     honeycomb structure.
     Add magnetic parts to the lattice and return the particle layout of it.
     '''
-    sf=(self.lambda_i*AA)**2/2./pi
-
     # particle SLD to produce a contrast between average density and particle of Py-SLD
     # external field direction
     B_ext=ba.kvector_t(0., 1., 0.)
     m_hole=ba.HomogeneousMagneticMaterial("PermalloyHole", 0., 0.,
                                           0.*B_ext)
-    m_full=ba.HomogeneousMagneticMaterial("PermalloyFull", SLD_Py*sf, 0.,
-                                          (self.surface_fraction-1.)*B_ext*self.bias_field*sf)
+    m_full=ba.HomogeneousMagneticMaterial("PermalloyFull", self.get_n('Py').real,
+                                          self.get_n('Py').imag,
+                                          (self.surface_fraction-1.)*B_ext*self.get_n('MagB'))
     ll=self.hc_lattice_length
     mll=self.mag_lattice_length
 
@@ -146,13 +195,13 @@ class HCSample(MLBuilder):
     basis=ba.ParticleComposition()
     particle_layout=ba.ParticleLayout()
     
-    if self.magneti_model=='ferro':
+    if self.basic_model in self.single_uc:
       hexagon=ba.Particle(m_full, hexagon_ff)
       cell=ba.ParticleCoreShell(hexagon, cylinder)
       # for pure ferromagnetism the unit cell can be reduced to one particle.
       basis.addParticles(cell, [o]) # nuclear unit cell
       particle_layout.addParticle(basis, 1.0, ba.kvector_t(0, 0, 0), ba.RotationZ((self.xi-30.)*deg))
-    else:
+    elif self.basic_model=='vortex':
       hexagon=ba.Particle(m_full, hexagon_ff)
       cell=ba.ParticleCoreShell(hexagon, cylinder)
       # vortex magnetic model
@@ -162,14 +211,20 @@ class HCSample(MLBuilder):
       basis.addParticles(cell, [o, o+ln_a, o+ln_b ]) # nuclear unit cell hole
       self.build_votex(basis) # magnetic unit cell
       particle_layout.addParticle(basis, 1.0, ba.kvector_t(0, 0, 0), ba.RotationZ((self.xi-30.)*deg))
+    else:
+      raise ValueError, 'Basic model %s not implemented'%self.basic_model
 
     interference=self.get_interference_function()
+    #pdf=ba.FTDistribution1DGauss(3*nm)
+    #interference.setProbabilityDistribution(pdf)
+    #interference.setKappa(1.0)
     particle_layout.addInterferenceFunction(interference)
+    #particle_layout.setApproximation(ba.ILayout.SSCA)
     return particle_layout
 
   def get_interference_function(self):
 
-    if self.magneti_model=='ferro':
+    if self.basic_model in self.single_uc:
       ll=self.hc_lattice_length
       rot=self.xi
     else:
@@ -188,18 +243,230 @@ class HCSample(MLBuilder):
       interference.setIntegrationOverXi(False)
     return interference
 
+  def build_glass(self, hexagon_density=0.0003):
+    '''
+    Generate a set of magnetic particles present in the spin-glass phase.
+    Here there is no correlation of magnetic moments, only preferred orientations.
+    '''
+    # 2in-1out and 1-in-2out states with 3-fold rotation possibilities
+    MagE=self.get_n('MagE')
+    ll=self.hc_lattice_length
+    r=self.hc_inner_radius
+    mll=self.mag_lattice_length
+
+    # magnetic element has width of narrowest region of Honeycomb
+    # and almost half the lattice length
+    spin_ff=ba.FormFactorBox(ll*0.4, ll-2.*r, self.py_d)
+
+    M0=ba.kvector_t(cos(self.xi*deg), sin(self.xi*deg), 0.)
+    m_mag_000=ba.HomogeneousMagneticMaterial("Spin", 0., 0., M0*MagE)
+    m_mag_180=m_mag_000
+    m_mag_030=m_mag_000
+    m_mag_240=m_mag_000
+    m_mag_330=m_mag_000
+    m_mag_120=m_mag_000
+
+    rotation=ba.RotationZ(0.*deg)
+    spin_1p=ba.Particle(m_mag_000, spin_ff, rotation)
+    rotation=ba.RotationZ(180.*deg)
+    spin_1m=ba.Particle(m_mag_180, spin_ff, rotation)
+
+    rotation=ba.RotationZ(30.*deg)
+    spin_2p=ba.Particle(m_mag_030, spin_ff, rotation)
+    rotation=ba.RotationZ(240.*deg)
+    spin_2m=ba.Particle(m_mag_240, spin_ff, rotation)
+
+    rotation=ba.RotationZ(330.*deg)
+    spin_3p=ba.Particle(m_mag_330, spin_ff, rotation)
+    rotation=ba.RotationZ(120.*deg)
+    spin_3m=ba.Particle(m_mag_120, spin_ff, rotation)
+
+    # there are 3 edges per hexagon unit cell (6 shared, each shared by 2 cells)
+    pos=ba.kvector_t(0.0, 0.0,-self.py_d)
+    PLs=[]
+    rot=ba.RotationZ(self.xi*deg)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(spin_1p, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/12.)
+    PLs.append(PLi)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(spin_1m, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/12.)
+    PLs.append(PLi)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(spin_2p, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/12.)
+    PLs.append(PLi)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(spin_2m, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/12.)
+    PLs.append(PLi)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(spin_3p, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/12.)
+    PLs.append(PLi)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(spin_3m, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/12.)
+    PLs.append(PLi)
+
+    return PLs
+
+  def build_ice1(self, hexagon_density=0.0003):
+    '''
+    Generate a set of magnetic particle bases present in the spin-ice 1 phase.
+    Here the nodes consist of 2in-1out and 1in-2out correlated tri-bonds.
+    '''
+    # 2in-1out and 1-in-2out states with 3-fold rotation possibilities
+    MagE=self.get_n('MagE')
+    ll=self.hc_lattice_length
+    r=self.hc_inner_radius
+    mll=self.mag_lattice_length
+
+    o=ba.kvector_t(0.0, 0.0,-self.py_d) # origin is the bottom of the py layer
+    ln_a=ba.kvector_t(cos(0.*deg)*ll, sin(00.*deg)*ll, 0.)
+    ln_b=ba.kvector_t(cos(60.*deg)*ll, sin(60.*deg)*ll, 0.)
+
+    # magnetic element has width of narrowest region of Honeycomb
+    # and almost half the lattice length
+    spin_ff=ba.FormFactorBox(ll*0.4, ll-2.*r, self.py_d)
+    basis1=ba.ParticleComposition()
+    basis2=ba.ParticleComposition()
+
+    M0=ba.kvector_t(cos(self.xi*deg), sin(self.xi*deg), 0.)
+    m_mag_000=ba.HomogeneousMagneticMaterial("Spin", 0., 0., M0*MagE)
+    m_mag_180=m_mag_000
+    m_mag_030=m_mag_000
+    m_mag_240=m_mag_000
+    m_mag_330=m_mag_000
+    m_mag_120=m_mag_000
+
+    rotation=ba.RotationZ(0.*deg)
+    spin_1p=ba.Particle(m_mag_000, spin_ff, rotation)
+    rotation=ba.RotationZ(180.*deg)
+    spin_1m=ba.Particle(m_mag_180, spin_ff, rotation)
+
+    rotation=ba.RotationZ(30.*deg)
+    spin_2p=ba.Particle(m_mag_030, spin_ff, rotation)
+    rotation=ba.RotationZ(240.*deg)
+    spin_2m=ba.Particle(m_mag_240, spin_ff, rotation)
+
+    rotation=ba.RotationZ(330.*deg)
+    spin_3p=ba.Particle(m_mag_330, spin_ff, rotation)
+    rotation=ba.RotationZ(120.*deg)
+    spin_3m=ba.Particle(m_mag_120, spin_ff, rotation)
+
+    basis1.addParticles(spin_1p, [o])
+    basis1.addParticles(spin_2p, [o+ln_a/2.])
+    basis1.addParticles(spin_3p, [o+ln_a/2.-ln_b/2.])
+
+    basis2.addParticles(spin_1m, [o])
+    basis2.addParticles(spin_2m, [o+ln_a/2.])
+    basis2.addParticles(spin_3m, [o+ln_a/2.-ln_b/2.])
+
+    # each hexagon has one of the 6 possible combinations per corner
+    pos=ba.kvector_t(0.0, 0.0, 0.0)
+    PLs=[]
+    for i in range(3):
+      rot=ba.RotationZ(120.*deg*i+self.xi*deg)
+      PLi=ba.ParticleLayout()
+      PLi.addParticle(basis1, 1.0, pos, rot)
+      PLi.setTotalParticleSurfaceDensity(hexagon_density/6.)
+      PLs.append(PLi)
+      PLi=ba.ParticleLayout()
+      PLi.addParticle(basis2, 1.0, pos, rot)
+      PLi.setTotalParticleSurfaceDensity(hexagon_density/6.)
+      PLs.append(PLi)
+    return PLs
+
+  def build_ice2(self, hexagon_density=0.0003):
+    '''
+    Generate a set of magnetic particle bases present in the spin-ice 2 phase.
+    Here there are uncorrelated left and right vortices of moments distributed over the lattice.
+    '''
+    # 2in-1out and 1-in-2out states with 3-fold rotation possibilities
+    MagE=self.get_n('MagE')
+    ll=self.hc_lattice_length
+    r=self.hc_inner_radius
+    mll=self.mag_lattice_length
+
+    o=ba.kvector_t(0.0, 0.0,-self.py_d) # origin is the bottom of the py layer
+    ln_a=ba.kvector_t(cos(0.*deg)*ll, sin(00.*deg)*ll, 0.)
+    ln_b=ba.kvector_t(cos(60.*deg)*ll, sin(60.*deg)*ll, 0.)
+
+    # magnetic element has width of narrowest region of Honeycomb
+    # and almost half the lattice length
+    spin_ff=ba.FormFactorBox(ll*0.4, ll-2.*r, self.py_d)
+    basis1=ba.ParticleComposition() # right handed
+    basis2=ba.ParticleComposition() # left handed
+
+    M0=ba.kvector_t(cos(self.xi*deg), sin(self.xi*deg), 0.)
+    m_mag_000=ba.HomogeneousMagneticMaterial("Spin", 0., 0., M0*MagE)
+    m_mag_180=m_mag_000
+    m_mag_030=m_mag_000
+    m_mag_240=m_mag_000
+    m_mag_330=m_mag_000
+    m_mag_120=m_mag_000
+
+    rotation=ba.RotationZ(0.*deg)
+    spin_1p=ba.Particle(m_mag_000, spin_ff, rotation)
+    rotation=ba.RotationZ(180.*deg)
+    spin_1m=ba.Particle(m_mag_180, spin_ff, rotation)
+
+    rotation=ba.RotationZ(30.*deg)
+    spin_2p=ba.Particle(m_mag_030, spin_ff, rotation)
+    rotation=ba.RotationZ(240.*deg)
+    spin_2m=ba.Particle(m_mag_240, spin_ff, rotation)
+
+    rotation=ba.RotationZ(330.*deg)
+    spin_3p=ba.Particle(m_mag_330, spin_ff, rotation)
+    rotation=ba.RotationZ(120.*deg)
+    spin_3m=ba.Particle(m_mag_120, spin_ff, rotation)
+
+    basis1.addParticles(spin_1p, [o+ln_b/2.])
+    basis1.addParticles(spin_1m, [o-ln_b/2.])
+    basis1.addParticles(spin_2p, [o+ln_a/2.])
+    basis1.addParticles(spin_2m, [o-ln_a/2.])
+    basis1.addParticles(spin_3p, [o+ln_a/2.-ln_b/2.])
+    basis1.addParticles(spin_3m, [o-ln_a/2.+ln_b/2.])
+
+    basis2.addParticles(spin_1p, [o-ln_b/2.])
+    basis2.addParticles(spin_1m, [o+ln_b/2.])
+    basis2.addParticles(spin_2p, [o-ln_a/2.])
+    basis2.addParticles(spin_2m, [o+ln_a/2.])
+    basis2.addParticles(spin_3p, [o-ln_a/2.+ln_b/2.])
+    basis2.addParticles(spin_3m, [o+ln_a/2.-ln_b/2.])
+
+    # only each 3rd hexagon can be left or right chirally ordered
+    pos=ba.kvector_t(0.0, 0.0, 0.0)
+    PLs=[]
+    rot=ba.RotationZ(self.xi*deg)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(basis1, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/3.)
+    PLs.append(PLi)
+    PLi=ba.ParticleLayout()
+    PLi.addParticle(basis2, 1.0, pos, rot)
+    PLi.setTotalParticleSurfaceDensity(hexagon_density/3.)
+    PLs.append(PLi)
+    return PLs
+
+
   def build_votex(self, basis):
     '''
     Add magnetic particles to the basis used in build_lattice conform to a vortex model.
     '''
-    sf=self.lambda_i**2/2./pi
+    MagE=self.get_n('MagE')
     ll=self.hc_lattice_length
+    r=self.hc_inner_radius
     mll=self.mag_lattice_length
 
-    spin_ff=ba.FormFactorBox(ll*0.4, 0.1*ll, self.py_d)
+    # magnetic element has width of narrowest region of Honeycomb
+    # and almost half the lattice length
+    spin_ff=ba.FormFactorBox(ll*0.4, ll-2.*r, self.py_d)
 
-    M=ba.kvector_t(cos(self.xi*deg), sin(self.xi*deg), 0.)*self.edge_field
-    m_mag_000=ba.HomogeneousMagneticMaterial("Spin", 0., 0., M*sf)
+    M0=ba.kvector_t(cos(self.xi*deg), sin(self.xi*deg), 0.)
+    m_mag_000=ba.HomogeneousMagneticMaterial("Spin", 0., 0., M0*MagE)
     m_mag_180=m_mag_000
     m_mag_030=m_mag_000
     m_mag_240=m_mag_000
@@ -237,4 +504,4 @@ class HCSample(MLBuilder):
     basis.addParticles(spin_3m, [o+ln_a*3./2.])
 
 if __name__=='__main__':
-  print "This is just a module containing the sample description, run HoneyComb.py to simulate."
+  print("This is just a module containing the sample description, run HoneyComb.py to simulate.")
